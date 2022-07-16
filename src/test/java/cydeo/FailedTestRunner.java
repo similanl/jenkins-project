4f096d772768220287c19dded13e0c7e1ed297b9
simil <similanl@hotmail.com>
2022-07-16 10:38:44 -0400

    Created a failedTestRunner class to only run the fail

---
+++ src/test/java/cydeo/FailedTestRunner.java
@@ -0,0 +1,16 @@
+package cydeo;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "@target/rerun.txt"
+
+)
+public class FailedTestRunner {
+
+
+}
